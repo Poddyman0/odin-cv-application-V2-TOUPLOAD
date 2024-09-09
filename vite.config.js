3d15504a3cbcf43857e2e3f1fa90c8061ae32c86
Poddyman0 <127142059+Poddyman0@users.noreply.github.com>
2024-09-09 15:12:34 +0100

    package json added

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
-import { defineConfig } from 'vite';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
+// https://vitejs.dev/config/
 export default defineConfig({
-  server: {
-    port: process.env.PORT || 3000,
-  }
-});
+  plugins: [react()],
+})
